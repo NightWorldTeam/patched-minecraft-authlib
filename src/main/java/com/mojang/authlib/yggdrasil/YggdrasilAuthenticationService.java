e881356c52792e7f998ef7825e84c583763e88a2
Igor <igor.shaposhnikov@urfu.me>
2024-06-21 17:19:49 +0500

    patching

--- src/main/java/com/mojang/authlib/yggdrasil/YggdrasilAuthenticationService.java
+++ src/main/java/com/mojang/authlib/yggdrasil/YggdrasilAuthenticationService.java
@@ -7,6 +7,15 @@ import com.mojang.authlib.HttpAuthenticationService;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.authlib.minecraft.UserApiService;
 import com.mojang.authlib.minecraft.client.MinecraftClient;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +28,46 @@ public class YggdrasilAuthenticationService extends HttpAuthenticationService {
 
     private final Environment environment;
     private final ServicesKeySet servicesKeySet;
+
+    static {
+        String defaultAgent = System.getProperty("http.agent");
+        if (defaultAgent == null || defaultAgent.toLowerCase().contains("java")) {
+            System.setProperty("http.agent", "Mozilla/5.0");
+        }
+
+        try {
+            domainTrust();
+        } catch (Exception e) { }
+    }
+
+    public static void domainTrust() throws NoSuchAlgorithmException, KeyManagementException {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+
+        // Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+    }
 
     public YggdrasilAuthenticationService(final Proxy proxy) {
         this(proxy, determineEnvironment());
